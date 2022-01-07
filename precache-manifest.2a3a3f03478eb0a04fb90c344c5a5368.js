06161fc6e8a8da08e16d72e1c34281909931d2c1
napatsorn <seesuwannapatsorn@gmail.com>
2022-01-07 16:09:59 +0700

    Updates

---
+++ precache-manifest.2a3a3f03478eb0a04fb90c344c5a5368.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8db621208a7f21d1851e7ce4787808c2",
+    "url": "/napatsorn24.github.io/project_map_test/index.html"
+  },
+  {
+    "revision": "aae71b7b1244a57bd5d3",
+    "url": "/napatsorn24.github.io/project_map_test/static/css/2.464e1e64.chunk.css"
+  },
+  {
+    "revision": "aae71b7b1244a57bd5d3",
+    "url": "/napatsorn24.github.io/project_map_test/static/js/2.838c67c7.chunk.js"
+  },
+  {
+    "revision": "b9cad080d309b6239d1150e1cbf7d33a",
+    "url": "/napatsorn24.github.io/project_map_test/static/js/2.838c67c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a513c4a18ddf60f5fd41",
+    "url": "/napatsorn24.github.io/project_map_test/static/js/main.1a6d90e9.chunk.js"
+  },
+  {
+    "revision": "42ab31bee731af8ec7cc",
+    "url": "/napatsorn24.github.io/project_map_test/static/js/runtime-main.2e0c0d81.js"
+  }
+]);
